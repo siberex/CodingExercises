8b1d0439bf820a7f16a3a2bed6881ecc58e8c09a
Stepan Legachev <gitlog@sib.li>
2021-08-31 01:08:31 +0800

    add TwoSum js implementation

---
+++ algorithms/TwoSum.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let pairIndex = nums.indexOf(target - nums[i]);
+    if (pairIndex !== -1 && pairIndex !== i)
+      return [i, pairIndex];
+  }
+  return null;
+};
+
+console.log(
+  twoSum([2,7,11,15], 9), // [0,1]
+  twoSum([3,2,4], 6),     // [1,2]
+  twoSum([3,3], 6),       // [0,1]
+  twoSum([3,3], 9),       // null
+)
