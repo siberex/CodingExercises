04f1b7c78db6c6f8a718be522a661e1ed30f0e9d
Stepan Legachev <gitlog@sib.li>
2021-09-01 08:48:08 +0800

    refactor: remove ton of reallocs
    leetcode tests:
    Runtime  Memory
    983 ms	 765 MB    ton of reallocs
    104 ms	 17.9 MB   no reallocs (current)
    
    no difference locally though
    ¯\_(ツ)_/¯

--- algorithms/less-common/findDuplicates.c
+++ algorithms/less-common/findDuplicates.c
@@ -3,11 +3,14 @@
 #include "findDuplicates.input.h"
 
 /**
+ * https://leetcode.com/problems/find-all-duplicates-in-an-array/submissions/
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDuplicates(int* nums, int numsSize, int* returnSize) {
   *returnSize = 0;
-  int *result = malloc(0);
+  // Worst-case: every other number is duplicate,
+  // therefore numsSize / 2 is sufficient
+  int *result = malloc(numsSize / 2 * sizeof(int));
 
   for (int i = 0; i < numsSize; ++i) {
     int n = nums[i] > 0 ? nums[i] : -nums[i];
@@ -15,12 +18,12 @@ int* findDuplicates(int* nums, int numsSize, int* returnSize) {
     if (nums[n - 1] < 0) {
       // We have met number = n previously
       *returnSize = *returnSize + 1;
-      result = realloc(result, *returnSize * sizeof(int));
       result[*returnSize - 1] = n;
     } else {
       nums[n - 1] = -nums[n - 1];
     }
   }
+  result = realloc(result, *returnSize * sizeof(int));
   return result;
 }
 
