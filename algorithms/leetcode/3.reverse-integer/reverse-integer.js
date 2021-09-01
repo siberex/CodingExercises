c80bc4551f4ae1165b6ad786dc92daac8b21c216
Stepan Legachev <gitlog@sib.li>
2021-09-01 10:51:04 +0800

    add less elegant and even slower solution

--- algorithms/leetcode/3.reverse-integer/reverse-integer.js
+++ algorithms/leetcode/3.reverse-integer/reverse-integer.js
@@ -6,7 +6,29 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
+var reverseNaive2 = function(x) {
+  const isNegative = x < 0;
+
+  // Eliminate sign
+  if (isNegative) x *= -1;
+
+  let reverse = x.toString().split('')
+    .reduce(
+        // ['1','2','3'] => 321
+        (acc, curr, i) => acc + 10 ** i * (curr|0),
+        0
+    );
+
+  // If reversing x causes the value to go outside the signed 32-bit integer range, then return 0
+  // 0x80000000 === 2^31
+  if (reverse > 0x80000000) return 0;
+
+  // Restore sign
+  if (isNegative) reverse *= -1;
+  return reverse;
+};
+
+let reverseNaive1 = function(x) {
   // This is WRONG SOLUTION: environment does not allow you to store 64-bit integers
   let reverse = parseInt(x.toString().split('').reverse().join(''));
   // If reversing x causes the value to go outside the signed 32-bit integer range, then return 0
