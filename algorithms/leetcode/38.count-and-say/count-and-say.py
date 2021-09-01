8841b50c05b5a2a5c3663f68ef55d1c9dc79f277
Stepan Legachev <gitlog@sib.li>
2021-09-02 05:05:53 +0800

    refactor: reformat this wonderful code golf

--- algorithms/leetcode/38.count-and-say/count-and-say.py
+++ algorithms/leetcode/38.count-and-say/count-and-say.py
@@ -6,7 +6,18 @@ import re
 
 def lookandsay(limit, sequence = "1"):
     if limit > 1:
-        return lookandsay(limit-1, "".join([str(len(match.group()))+match.group()[0] for matchNum, match in enumerate(re.finditer(r"(\w)\1*", str(sequence)))]))
+        return lookandsay(
+            limit - 1,
+            "".join(
+                [
+                    str( len(match.group()) ) + match.group()[0]
+                        for matchNum, match
+                        in enumerate(
+                            re.finditer(r"(\w)\1*", str(sequence))
+                        )
+                ]
+            )
+        )
     else:
         return sequence
 # lookandsay(3) --> 21
