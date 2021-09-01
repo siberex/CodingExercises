63bbc1c1c801abff8b96154b3d198cd4baa91c87
Stepan Legachev <gitlog@sib.li>
2021-09-01 08:34:58 +0800

    refactor: return ton of reallocs

--- algorithms/less-common/findDuplicates.c
+++ algorithms/less-common/findDuplicates.c
@@ -1,46 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "findDuplicates.input.h"
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDuplicates(int* nums, int numsSize, int* returnSize) {
   int resultSize = 0;
-  // hmm, this looks like shit
-  int *result = malloc(numsSize * sizeof(int));
+  int *result = malloc(0);
 
-  int n;
   for (int i = 0; i < numsSize; ++i) {
-    n = nums[i] > 0 ? nums[i] : -nums[i];
+    int n = nums[i] > 0 ? nums[i] : -nums[i];
 
     if (nums[n - 1] < 0) {
       // We have met number = n previously
       ++resultSize;
+      result = realloc(result, resultSize * sizeof(int));
       result[resultSize - 1] = n;
     } else {
       nums[n - 1] = -nums[n - 1];
     }
   }
   *returnSize = resultSize;
-  result = realloc(result, resultSize * sizeof(int));
   return result;
 }
 
 
-// gcc -ansi -o findDuplicates findDuplicates.c && ./findDuplicates
-// gcc -std=c11 -Wall -o findDuplicates findDuplicates.c && ./findDuplicates
+// gcc -std=c11 -Wall -o /tmp/findDuplicates findDuplicates.c && time /tmp/findDuplicates
+// clang -std=c11 -Wall -o /tmp/findDuplicates findDuplicates.c && /tmp/findDuplicates
 int main() {
   int resSize = 0;
-  int data[] = {5,4,6,7,9,3,10,9,5,6};
-  size_t dataSize = sizeof data / sizeof *data;
+//  int data[] = {5,4,6,7,9,3,10,9,5,6};
+  size_t dataSize = sizeof INPUT_DATA / sizeof *INPUT_DATA;
 
-  int* dups = findDuplicates(&data[0], dataSize, &resSize);
+  int* dups = findDuplicates(&INPUT_DATA[0], dataSize, &resSize);
 
-  printf("\nresults count: %d\n",resSize);
+  printf("Duplicates Count: %d\n",resSize);
   for (int i = 0; i < resSize; ++i) {
     printf("%d ", dups[i]);
   }
   free(dups);
 
+  printf("\n");
   return 0;
 }
