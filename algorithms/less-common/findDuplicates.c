013f44cc177a19658a196c599a3bad17efba4b28
Stepan Legachev <gitlog@sib.li>
2021-09-01 08:41:04 +0800

    refactor: access pointer directly

--- algorithms/less-common/findDuplicates.c
+++ algorithms/less-common/findDuplicates.c
@@ -6,7 +6,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDuplicates(int* nums, int numsSize, int* returnSize) {
-  int resultSize = 0;
+  *returnSize = 0;
   int *result = malloc(0);
 
   for (int i = 0; i < numsSize; ++i) {
@@ -14,14 +14,13 @@ int* findDuplicates(int* nums, int numsSize, int* returnSize) {
 
     if (nums[n - 1] < 0) {
       // We have met number = n previously
-      ++resultSize;
-      result = realloc(result, resultSize * sizeof(int));
-      result[resultSize - 1] = n;
+      *returnSize = *returnSize + 1;
+      result = realloc(result, *returnSize * sizeof(int));
+      result[*returnSize - 1] = n;
     } else {
       nums[n - 1] = -nums[n - 1];
     }
   }
-  *returnSize = resultSize;
   return result;
 }
 
@@ -30,7 +29,6 @@ int* findDuplicates(int* nums, int numsSize, int* returnSize) {
 // clang -std=c11 -Wall -o /tmp/findDuplicates findDuplicates.c && /tmp/findDuplicates
 int main() {
   int resSize = 0;
-//  int data[] = {5,4,6,7,9,3,10,9,5,6};
   size_t dataSize = sizeof INPUT_DATA / sizeof *INPUT_DATA;
 
   int* dups = findDuplicates(&INPUT_DATA[0], dataSize, &resSize);
