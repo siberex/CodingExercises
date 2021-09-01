685f52a64f7619124ba010283a578d3c0aed508e
Stepan Legachev <gitlog@sib.li>
2021-09-01 12:16:22 +0800

    add correct solution

--- algorithms/leetcode/3.reverse-integer/reverse-integer.js
+++ algorithms/leetcode/3.reverse-integer/reverse-integer.js
@@ -6,6 +6,37 @@
  * @param {number} x
  * @return {number}
  */
+var reverse = function(x) {
+  // Guaranteed overflow for n >= 2**31 and n < -2**31
+  let int32 = new Int32Array(1);
+
+  const isNegative = x < 0;
+
+  // Eliminate sign
+  if (isNegative) x *= -1;
+
+  // Note reverse()
+  const arr = x.toString().split('').reverse();
+
+  // ['3','2','1'] => 321
+  for (const i in arr) {
+    const digit = arr[i] | 0; // aka parseInt
+
+    // Make sure n <= 0x7FFFFFFF (= 2**31 - 1)
+    // 214748364 === (2**31 / 10 | 0)
+    if (int32[0] > 214748364) return 0;
+    int32[0] *= 10;
+
+    // 2**31 - 214748364 * 10 === 8
+    if (int32[0] == 0x7FFFFFFF && digit > 7) return 0;
+    int32[0] += digit;
+  }
+
+  // Restore sign
+  if (isNegative) int32[0] *= -1;
+  return int32[0];
+};
+
 var reverseNaive2 = function(x) {
   const isNegative = x < 0;
 
@@ -20,7 +51,7 @@ var reverseNaive2 = function(x) {
     );
 
   // If reversing x causes the value to go outside the signed 32-bit integer range, then return 0
-  // 0x80000000 === 2^31
+  // 0x80000000 === 2**31
   if (reverse > 0x80000000) return 0;
 
   // Restore sign
@@ -32,7 +63,7 @@ let reverseNaive1 = function(x) {
   // This is WRONG SOLUTION: environment does not allow you to store 64-bit integers
   let reverse = parseInt(x.toString().split('').reverse().join(''));
   // If reversing x causes the value to go outside the signed 32-bit integer range, then return 0
-  if (reverse > 0x80000000) return 0; // 0x80000000 === 2^31
+  if (reverse > 0x80000000) return 0; // 0x80000000 === 2**31
   if (x < 0) reverse *= -1;
   return reverse;
 };
