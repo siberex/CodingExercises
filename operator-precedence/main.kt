92a36caf53540ea5148ead45eff55a1c8403e78c
Stepan Legachev <gitlog@sib.li>
2022-01-02 06:58:50 +0800

    docs: args to compile with kotlin-native and with GraalVM native-image

--- operator-precedence/main.kt
+++ operator-precedence/main.kt
@@ -1,4 +1,8 @@
 // kotlinc -nowarn main.kt -include-runtime -d /tmp/main.jar && java -jar /tmp/main.jar
+// native-image -jar /tmp/main.jar /tmp/main
+
+// xattr -rd com.apple.quarantine /usr/local/Caskroom/kotlin-native
+// kotlinc-native -nowarn -opt main.kt -o /tmp/main && /tmp/main.kexe
 fun main() {
     var year = 2021
     println(
