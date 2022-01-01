f2fe92385cd967f9ef4b61df76ddb595493fbaa4
Stepan Legachev <gitlog@sib.li>
2022-01-02 07:24:17 +0800

    docs: GraalVM native-image

--- operator-precedence/main.kt
+++ operator-precedence/main.kt
@@ -1,5 +1,6 @@
 // kotlinc -nowarn main.kt -include-runtime -d /tmp/main.jar && java -jar /tmp/main.jar
-// native-image -jar /tmp/main.jar /tmp/main
+// native-image --pgo-instrument -jar /tmp/main.jar
+// native-image --pgo -jar /tmp/main.jar
 
 // xattr -rd com.apple.quarantine /usr/local/Caskroom/kotlin-native
 // kotlinc-native -nowarn -opt main.kt -o /tmp/main && /tmp/main.kexe
